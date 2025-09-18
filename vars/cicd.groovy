1548183038f062ffb6e4b5bfa74b6c0e427b1023
ganeshgoud43 <ganeshgoud43@gmail.com>
2025-09-18 18:23:15 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def gitBuild()
 {
   sh "mvn package"
 }
+def gitDeploy(jobname,ipaddress,context)
+{
+  sh "/var/lib/jenkins/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context}.war"
+}
